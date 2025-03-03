6606f0474a4ce06cfdc687334bac9f3686278036
jhosenthG <jhosenth@gmail.com>
2025-03-03 14:55:41 -0400

    Initial commit

---
+++ app/src/main/java/com/example/obragris/domain/entities/InformationProject.kt
@@ -0,0 +1,10 @@
+package com.example.obragris.domain.entities
+
+data class InformationProject(
+    val idProject: Int,
+    val nameProject: String,
+    val descriptionProject: String,
+    val startDateProject: String,
+    val endDateProject: String,
+    val locationProject: String
+)
