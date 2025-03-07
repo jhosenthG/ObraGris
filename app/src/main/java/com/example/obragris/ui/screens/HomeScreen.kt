c9cc3b4c1f7fa16c6ef805748152130d9c55316d
jhosenthG <jhosenth@gmail.com>
2025-03-07 18:13:32 -0400

    Refactor: Add new colors and screens and refactor HomeScreen
    - Add new colors: BlueGray, GrayMedium, GreenApple, and LightGrey.
    - Add new screens: ReportFormScreen, ReportTaskScreen, ListTaskReport, and ListReportScreen.
    - Create HomeScreenState and HomeScreenViewModel.
    - Rename HomeScreen file.

--- app/src/main/java/com/example/obragris/ui/screens/HomeScreen.kt
+++
@@ -1,9 +0,0 @@
-package com.example.obragris.ui.screens
-
-import androidx.compose.runtime.Composable
-import com.example.obragris.ui.components.cardProjectComponent
-
-@Composable
-fun homeScreen() {
-    cardProjectComponent()
-}
---
+++ app/src/main/java/com/example/obragris/ui/screens/HomeScreen/HomeScreen.kt
@@ -0,0 +1,7 @@
+package com.example.obragris.ui.screens.HomeScreen
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun homeScreen() {
+}
