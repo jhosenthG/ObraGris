573fea7d1408c3acb9744b27272c65a8970e1b2d
JhosenthG <jhosenth@gmail.com>
2025-03-22 15:57:57 -0400

    change file for navigation

--- app/src/main/java/com/example/obragris/ui/components/BottomBarComponent.kt
+++ app/src/main/java/com/example/obragris/ui/components/BottomBarComponent.kt
@@ -1,14 +1,9 @@
 package com.example.obragris.ui.components
 
-import android.R
-import android.graphics.Color
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import com.example.obragris.ui.theme.White
 
 
 @Composable
-fun BottomBarComponent(
-    modifier: Modifier = Modifier,
-) {
+fun BottomBarComponent() {
+
 }
