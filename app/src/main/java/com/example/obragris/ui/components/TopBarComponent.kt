0756c76dcb9cb173fc3ee2042d2bf31f45d495b5
jhosenthG <jhosenth@gmail.com>
2025-03-24 13:56:40 -0400

    update the JDK version and create top bar component

--- app/src/main/java/com/example/obragris/ui/components/TopBarComponent.kt
+++ app/src/main/java/com/example/obragris/ui/components/TopBarComponent.kt
@@ -1,2 +1,12 @@
 package com.example.obragris.ui.components
 
+import android.media.Image
+import androidx.compose.runtime.Composable
+
+@Composable
+fun TopBarComponent(
+    title: String,
+    image: Image
+) {
+
+}
